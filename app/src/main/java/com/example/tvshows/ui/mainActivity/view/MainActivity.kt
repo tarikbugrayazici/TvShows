b72f070ec5f79ec8b790fb209e610a3420bf3f8c
tarikbugrayazici <tarikbyazici61@gmail.com>
2021-03-21 20:15:39 +0300

    second commit

--- app/src/main/java/com/example/tvshows/ui/mainActivity/view/MainActivity.kt
+++ app/src/main/java/com/example/tvshows/ui/mainActivity/view/MainActivity.kt
@@ -54,10 +54,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initScrollListener() {
         recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-            }
-
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 if (!isLoadingShowed) {
